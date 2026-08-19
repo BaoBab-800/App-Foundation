9f0e2c25d8fcfed358e4370acb8c5f6af0bb3c46
Denchik <rock333shadow@gmail.com>
2026-08-19 16:11:14 +0300

    Added AppTheme class, which converts palette to ThemeData.

--- lib/app_kit.dart
+++ lib/app_kit.dart
@@ -1,3 +1,4 @@
 library app_kit;
 
 export 'src/theme/app_palette.dart';
+export 'src/theme/app_theme.dart';
---
+++ lib/src/theme/app_theme.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'app_palette.dart';
+
+/// Flutter ThemeData palette configuration converter
+/// Accepts a palette abstraction and uses it to assemble standard Flutter ThemeData objects.
+class AppTheme {
+  final AppPalette palette;
+
+  const AppTheme({
+    required this.palette,
+  });
+
+  ThemeData get light {
+    return ThemeData(
+      brightness: Brightness.light,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: palette.primary,
+        brightness: Brightness.light,
+      ),
+    );
+  }
+
+  ThemeData get dark {
+    return ThemeData(
+      brightness: Brightness.dark,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: palette.primary,
+        brightness: Brightness.dark,
+      ),
+    );
+  }
+}
